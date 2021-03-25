e2adc1808f4c6ec0db9d06483f08ad69da7213f0
juanBautista <juanbautista19@gmail.com>
2021-03-25 13:02:24 +0100

    no message

--- Functional-Programming-Project/src/section4/LambdaPractice3.java
+++ Functional-Programming-Project/src/section4/LambdaPractice3.java
@@ -7,6 +7,15 @@ public class LambdaPractice3 {
         IStringLength stringLength = str -> str.length();
         int length = stringLength.getLength("Hello");
         System.out.println(length);
+
+        IStringLength stringLength1 =
+                str -> {
+                    int lenght = str.length();
+                    System.out.println(lenght);
+                    return lenght;
+                };
+
+        stringLength1.getLength("Hello");
     }
 
 }
